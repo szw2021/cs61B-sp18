057fc623e8da84be3d9bce49be04ac43350fbc1b
shizhiwei <szw2016run@gmail.com>
2022-11-11 19:32:31 +0800

    finish ArrayDeque

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,91 @@
+public class ArrayDeque<T> {
+
+    private T[] Deque;
+    private int next = 4;
+    private int prev = 3;
+    private int size = 0;
+    private int count = 8;
+
+
+    public ArrayDeque() {
+        Deque = (T[]) new Object[8];
+
+    }
+
+    public void resize() {
+        T[] newDeque = (T[]) new Object[count * 2];
+        int first = (prev + 1) % size;
+        for (int i = 0; i < size; i++) {
+            newDeque[i] = Deque[first];
+            first = (first + 1) % size;
+        }
+        Deque = newDeque;
+        count = count * 2;
+        next = size;
+        prev = count - 1;
+    }
+
+    public void addFirst(T item) {
+
+        if (size == count) {
+            resize();
+        }
+        size++;
+        Deque[prev--] = item;
+        if (prev < 0) {
+            prev += count;
+        }
+    }
+
+    public void addLast(T item) {
+        if (size == count) {
+            resize();
+        }
+        size++;
+        Deque[next++] = item;
+        if (next >= count) {
+            next -= count;
+        }
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        int first = (prev + 1) % count;
+        for (int i = 0; i < size; i++) {
+            System.out.println(Deque[first]);
+            first = (first + 1) % count;
+        }
+    }
+
+    public T removeFirst() {
+        ++prev;
+        if (prev >= count) {
+            prev -= count;
+        }
+        T item = Deque[prev];
+        return item;
+    }
+
+
+    public T removeLast() {
+        --next;
+        if (next < 0) {
+            next += count;
+        }
+        T item = Deque[next];
+        return item;
+    }
+
+    public T get(int index) {
+        int first = (prev + 1) % count;
+        return Deque[(index + first) % count];
+    }
+
+}
---
+++ proj1a/ArrayDequeTest.java
@@ -0,0 +1,16 @@
+public class ArrayDequeTest {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        deque.addFirst(3);
+        deque.addFirst(2);
+        deque.addFirst(1);
+        deque.addLast(4);
+        deque.addLast(5);
+        deque.addLast(6);
+        deque.addLast(7);
+        deque.addLast(8);
+        deque.addFirst(0);
+//        deque.addLast(9);
+        deque.printDeque();
+    }
+}
