84e1a2c6611378e8523ee0639b60fba61f5bc0dc
shizhiwei <szw2016run@gmail.com>
2022-11-12 12:44:20 +0800

    finish TestGold

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,53 @@
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestArrayDequeGold {
+
+    StudentArrayDeque<Integer> studentArrayDeque = new StudentArrayDeque();
+    ArrayDequeSolution<Integer> arrayDequeSolution = new ArrayDequeSolution<>();
+
+    @Test
+    public void test() {
+
+        int num = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 10000; i += 1) {
+            double numberBetweenZeroAndOne = StdRandom.uniform();
+            if (numberBetweenZeroAndOne < 0.25) {
+                num++;
+                studentArrayDeque.addFirst(i);
+                arrayDequeSolution.addFirst(i);
+                sb.append("addFirst(").append(i).append(")\n");
+                assertEquals(sb.toString(), studentArrayDeque.get(0), arrayDequeSolution.get(0));
+            } else if (numberBetweenZeroAndOne < 0.5){
+                num++;
+                studentArrayDeque.addLast(i);
+                arrayDequeSolution.addLast(i);
+                sb.append("addLast(").append(i).append(")\n");
+                assertEquals(sb.toString(), studentArrayDeque.get(num - 1), arrayDequeSolution.get(num - 1));
+            } else if (numberBetweenZeroAndOne < 0.75) {
+                if (num == 0) {
+                    continue;
+                }
+                num--;
+                Integer stu = studentArrayDeque.removeFirst();
+                Integer sol = arrayDequeSolution.removeFirst();
+                sb.append("removeFirst()\n");
+                assertEquals(sb.toString(), stu, sol);
+            } else {
+                if (num == 0) {
+                    continue;
+                }
+                num--;
+                Integer stu = studentArrayDeque.removeLast();
+                Integer sol = arrayDequeSolution.removeLast();
+                sb.append("removeLast()\n");
+                assertEquals(sb.toString(), stu, sol);
+            }
+        }
+
+    }
+
+}
